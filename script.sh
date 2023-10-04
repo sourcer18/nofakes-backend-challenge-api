c53168c3b3641f122b6e882f3d488d7fffa81aee
David <davidsalascastro@gmail.com>
2023-10-04 19:10:21 +0200

    different environments in docker and local

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@
 echo "Comenzando ejecución de la aplicación..."
 echo "Instalo todas las dependencias..."
 npm install
+npm install --save-dev dotenv-cli
 npm install nodemon --save-dev
 echo "Costruyendo app..."
 npm run build
